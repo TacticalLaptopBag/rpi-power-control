657c466a1883ab04520ef62aba45345ea71309f9
TacticalLaptopBag <lightpower26@hotmail.com>
2024-05-14 01:49:13 -0400

    Properly running `npm install`

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-npm run install --omit=dev
+npm install --omit=dev
 npm run build
 cp -rfv dist/ rpi-power-control
 cp -v package.json rpi-power-control
@@ -10,7 +10,7 @@ cp -v package-lock.json rpi-power-control
 cp -v .env rpi-power-control
 
 cd frontend
-npm run install --omit=dev
+npm install --omit=dev
 npm run build
 cp -rfv dist/frontend/browser/* ../rpi-power-control/app
 
