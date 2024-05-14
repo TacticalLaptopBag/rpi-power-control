610b0b01b385056c426366aeaf9b9e585af4efe4
TacticalLaptopBag <lightpower26@hotmail.com>
2024-05-14 01:55:26 -0400

    Clean up

--- package.sh
+++ package.sh
@@ -10,6 +10,7 @@ cp -v package.json rpi-power-control
 cp -v package-lock.json rpi-power-control
 cp -v .env rpi-power-control
 
+echo
 echo "Building frontend..."
 cd frontend
 npm install
@@ -17,10 +18,18 @@ npm run build
 mkdir ../rpi-power-control/app
 cp -rfv dist/frontend/browser/* ../rpi-power-control/app
 
+echo
 echo "Downloading production dependencies..."
 cd ../rpi-power-control
 npm install --omit=dev
 
+echo
 echo "Packaging..."
 cd ..
 tar -cvf rpi-power-control.tar rpi-power-control/
+
+echo
+echo "Cleaning up..."
+rm -rfv rpi-power-control
+
+echo "Done!"
