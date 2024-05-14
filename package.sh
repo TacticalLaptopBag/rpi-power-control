1332165e04f83094eafb773768afccd532bdb694
TacticalLaptopBag <lightpower26@hotmail.com>
2024-05-14 01:51:29 -0400

    Fixed install omit

--- package.sh
+++ package.sh
@@ -2,17 +2,24 @@
 
 set -e
 
-npm install --omit=dev
+echo "Building backend..."
+npm install
 npm run build
 cp -rfv dist/ rpi-power-control
 cp -v package.json rpi-power-control
 cp -v package-lock.json rpi-power-control
 cp -v .env rpi-power-control
 
+echo "Building frontend..."
 cd frontend
-npm install --omit=dev
+npm install
 npm run build
 cp -rfv dist/frontend/browser/* ../rpi-power-control/app
 
+echo "Downloading production dependencies..."
+cd ../rpi-power-control
+npm install --omit=dev
+
+echo "Packaging..."
 cd ..
 tar -cvf rpi-power-control.tar rpi-power-control/
